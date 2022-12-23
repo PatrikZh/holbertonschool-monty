9de68dd6fdb8dc291b4cfec66ec651a697a1fa48
Denio1212 <shehuden22@yahoo.com>
2022-12-23 06:07:51 -0800

    add

--- mormonty.c
+++ mormonty.c
@@ -71,7 +71,7 @@ void add(stack_t **stack, unsigned int line_number)
 {
 	stack_t *tmp;
 
-	if (!*stack || !(*stack)->next);
+	if (!*stack || !(*stack)->next)
 	{
 		fprintf(stderr, "L%d: can't  add, stack too short\n", line_number);
 		freer_f(stack, line_number);
